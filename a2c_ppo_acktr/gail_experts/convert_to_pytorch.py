b00cfe4b2a0e3798f8d23f92323e3f469ac5c351
slee01 <slee01@snu.ac.kr>
2020-07-17 05:14:23 +0900

    modify h5f functions to corresponding pt functions

--- a2c_ppo_acktr/gail_experts/convert_to_pytorch.py
+++ a2c_ppo_acktr/gail_experts/convert_to_pytorch.py
@@ -1,8 +1,9 @@
 import argparse
 import os
 import sys
+import pickle
+import glob
 
-import h5py
 import numpy as np
 import torch
 
@@ -10,6 +11,7 @@ import torch
 python convert_to_pytorch.py --h5-file trajs_halfcheetah.h5
 python convert_to_pytorch.py --pkl-file Baxter_0.pkl | Cursor_7.pkl | Saywer_7.pkl
 """
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -27,9 +29,41 @@ def main():
     args = parser.parse_args()
 
     if args.pt_file is None:
-        args.pt_file = os.path.splitext(args.h5_file)[0] + '.pt'
+        args.pt_file = os.path.splitext(args.pkl_file)[0] + '.pt'
 
-    with h5py.File(args.h5_file, 'r') as f:
+    # define numpy arrays
+    states,  actions, rewards = [], [], []
+    # now load the picked numpy arrays
+    # for file_path in demo_files:
+    with open(args.pkl_file, "rb") as f:
+        demo = pickle.load(f)
+        print("demo: ", type(demo), demo.keys())
+        print("demo.qpos: ", type(demo['qpos']))
+        for i in range(len(demo['qpos'])):
+            print("i: ", i, demo['qpos'][i])
+        print("demo.actions: ", type(demo['actions']))
+        # add observations
+        for state in demo["obs"]:
+            states.append(state)
+        states.pop()
+
+        # add actions
+        for action in demo["actions"]:
+            actions.append(action)
+
+        # add rewards
+        if "rewards" in demo:
+            for reward in demo["rewards"]:
+                    rewards.append(reward)
+
+        dataset_size = len(states)
+        print("data: ", args.pkl_file)
+        print("dataset_size: ",  dataset_size)
+        print("states: ", np.array(states).shape)
+        print("actions: ", np.array(actions).shape)
+        print("rewards: ", np.array(rewards).shape)
+
+    with h5py.File(args.pkl_file, 'r') as f:
         dataset_size = f['obs_B_T_Do'].shape[0]  # full dataset size
 
         states = f['obs_B_T_Do'][:dataset_size, ...][...]
